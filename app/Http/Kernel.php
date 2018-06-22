2fa4c6b97be2e301bd4da68d9419d24989f9dbcd
Omar <Omar.blanco.37@hotmail.com>
2018-06-22 12:21:02 -0500

    Multi-Auth
    Se Realizo la Multi-Auth, se corrigio el problema de la base de datos por defecto cambiando el cotejamiento a UTF8 , tambien se modifico el welcome.blade

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -51,6 +51,10 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
+        'usuario' => \App\Http\Middleware\RedirectIfNotUsuario::class,
+        'usuario.guest' => \App\Http\Middleware\RedirectIfUsuario::class,
+        'admin' => \App\Http\Middleware\RedirectIfNotAdmin::class,
+        'admin.guest' => \App\Http\Middleware\RedirectIfAdmin::class,
         'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
